4cee213ee2182d910adcc020251d03d07c56ca8d
TowafH <towafh@nycstudents.net>
2024-07-19 16:00:50 -0400

    Added to stop draw()

--- sketch.js
+++ sketch.js
@@ -82,7 +82,45 @@ function setup() {
 
 
 function draw() {  // changed indentation so everything is following after the if one == true statement
-    if(One == true) {
+    if(trashScore>=10){
+        //Completion Screen
+        background(135,206,235);
+        image(bg, 0, 0, 500, 800);
+        //Scuba Img
+        image(scubaImg, 235, 10, 50, 50);
+        textSize(22);
+        stroke(1);
+        text("Ocean Crew", 345, 120, 300, 100);
+
+        //Seperation line from Top
+        fill(255)
+        rect(100, 100, 800, 10);
+
+//Box for Level Completed
+        fill(255);
+        rect(250, 200, 300, 100);
+
+//Text within Level Completed Box
+        fill(0);
+        textSize(30);
+        stroke(1);
+        text("Level Completed!!", 280, 240, 300, 100);
+
+        //Box for Congratulations
+        fill(255);
+        rect(250, 350, 300, 100);
+
+        //Text within Congratulations Box
+        fill(0);
+        textSize(30);
+        stroke(1);
+        text("Congratulations!", 280, 340, 300, 50);
+        textSize(25);
+        text("You saved the Ocean", 275, 380, 300, 30);
+        noLoop()
+
+    }
+    else if(One == true) {
         console.log("Inside level1");
         background(135,206,235)
         image(bg, 0, 0, 500, 800);
